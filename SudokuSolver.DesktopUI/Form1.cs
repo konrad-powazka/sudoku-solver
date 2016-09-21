bd2dfd93d7332a69ea9673bd039210c7f14f5c21
konrad-powazka <konrad.powazka@gmail.com>
2016-09-21 10:10:11 +0200

    Split projects, added test images

---
+++ SudokuSolver.DesktopUI/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SudokuSolver.DesktopUI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() != DialogResult.OK)
+            {
+                return;
+            }
+
+            using (var fileStream = openFileDialog1.OpenFile())
+            using (var image = new Bitmap(fileStream))
+            {
+                var solver = new SudokuPhotoSolver();
+                var solution = solver.SolveSudokuPhoto(image);
+                var message = solution?.ToString() ?? "Solving failed";
+
+                MessageBox.Show(message);
+            }
+        }
+    }
+}
